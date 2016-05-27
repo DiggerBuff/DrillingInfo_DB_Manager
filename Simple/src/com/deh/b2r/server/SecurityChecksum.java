55bdbdcceefe806982eb5287f89c73cba61b6655
Adam Curtiss <acurtiss@bb136-11.mines.edu>
2016-05-27 15:27:16 -0600

    Pulling Alans code for get and post with JAR files

--- Simple/src/com/deh/b2r/server/SecurityChecksum.java
+++ Simple/src/com/deh/b2r/server/SecurityChecksum.java
@@ -1,13 +1,26 @@
 package com.deh.b2r.server;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+//These allow for conversion of File object to byte[]
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+
+
 public class SecurityChecksum {
-	public static String hashPassword(String password) throws NoSuchAlgorithmException{
+	public static String hashPassword(File jarFile) throws NoSuchAlgorithmException{
+		
+	//	FileInputStream fis = new FileInputStream(new File("foo"));
+		
+		Path path = Paths.get("path/to/file");
+		byte[] data = Files.readAllBytes(jarFile);
 		
 		MessageDigest md = MessageDigest.getInstance("MD5");
-		md.update(password.getBytes());
+		md.update(jarFile.getBytes());
 		byte[] b = md.digest();
 		StringBuffer sb = new StringBuffer();
 		for(byte b1 : b){
