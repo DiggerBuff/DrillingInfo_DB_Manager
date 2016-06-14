717cc7702a230128ba79a651e70f43a496a0211a
Alan Champion <alanchampion@gmail.com>
2016-06-14 15:33:59 -0600

    Created my own html part. Got the scan and download working.

---
+++ DrillingInfoUI/WebContent/alan.js
@@ -0,0 +1,66 @@
+function updater(){
+	if(document.getElementById("updateButton").innerHTML != "Update") {
+		scanUpdates();
+	}
+	else {
+		getUpdates();
+	}
+}
+
+function scanUpdates() {
+	document.getElementById("updates").innerHTML = "<option>--None--</option>";
+
+	var xmlHttp = new XMLHttpRequest();
+	var url = "http://138.67.186.222:9898/";
+
+	xmlHttp.onreadystatechange = function () {
+		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+			document.getElementById("info1").innerHTML = "Updates found.";
+			var jars = JSON.parse(xmlHttp.responseText);
+			addUpdates(jars);
+		}
+		else if (xmlHttp.readyState == 4 && xmlHttp.status == 204) {
+			document.getElementById("info1").innerHTML = "No updates found.";
+		}
+	}
+
+	xmlHttp.open("GET", url, true);
+	xmlHttp.send();
+	document.getElementById("info1").innerHTML = "Scanning for updates..."
+}
+
+function addUpdates(jars) {
+	document.getElementById("updates").innerHTML = "<option>--All Updates--</option>";
+	var select = document.getElementById("updates"); 
+	for(var i = 0; i < jars.length; i++) 
+	{
+	    var jar = jars[i];
+	    var el = document.createElement("option");
+	    el.textContent = jar.name;
+	    el.value = jar.name;
+	    select.appendChild(el);
+	}
+	document.getElementById("updateButton").innerHTML = "Update";
+}
+
+function getUpdates() {
+	//alert("Sending request");
+	var xmlHttp = new XMLHttpRequest();
+	var url = "http://138.67.186.222:9898/replace/";
+
+	//alert("Sent request");
+	xmlHttp.onreadystatechange = function () {
+		//alert("State change");
+		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+			document.getElementById("info1").innerHTML = "Updates downloaded.";
+		}
+		else if (xmlHttp.readyState == 4 && xmlHttp.status >= 400) {
+			document.getElementById("info1").innerHTML = "Error downloading updates.";
+		}
+	}
+
+	xmlHttp.open("GET", url, true);
+	xmlHttp.send();
+
+	//alert("ended");
+}
